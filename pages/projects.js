b47048e4d9c2e0ce317f3527cdd288a76e61481f
Legoraft <50689727+legoraft@users.noreply.github.com>
2023-05-31 18:13:52 +0200

    added projects page

--- pages/projects.js
+++ pages/projects.js
@@ -1,5 +1,26 @@
-export default function Projects() {
+import { getSortedProjectsData } from "@/lib/projects";
+
+export default function Projects({ allProjectsData }) {
   return(
-    <div>Hello, projects!</div>
+    <div className="mx-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      {allProjectsData.map(({ frontmatter, id, date, status, title }) => (
+        <div key={id} className="m-4 border-2 border-slate-800 rounded-xl flex flex-col">
+          <h1 className="px-4 pt-4 pb-2">{title}</h1>
+          <h2 className={`px-4 pb-4 
+          ${status == "Ongoing" ? "text-orange-500" : "text-red-500" | status == "Needs deco" ? "text-yellow-500" :  "text-red-500" | status == "Done" ? "text-green-500" : "text-red-500"}`}>
+            <b>{status}</b>
+          </h2>
+        </div>
+      ))}
+    </div>
   );
+}
+
+export async function getStaticProps() {
+  const allProjectsData = getSortedProjectsData();
+  return {
+    props: {
+      allProjectsData,
+    },
+  };
 }
