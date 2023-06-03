dcfd68f45acba27e89625c1e039ac23fe0d60bc6
Legoraft <50689727+legoraft@users.noreply.github.com>
2023-06-03 08:32:58 +0200

    added link to project pages

--- pages/projects.js
+++ pages/projects.js
@@ -1,11 +1,12 @@
 import { getSortedProjectsData } from "@/lib/projects";
+import Link from "next/link";
 
 export default function Projects({ allProjectsData }) {
   return(
     <div className="mx-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-      {allProjectsData.map(({ frontmatter, id, date, status, title }) => (
+      {allProjectsData.map(({ id, status, title }) => (
         <div key={id} className="m-4 border-2 border-slate-800 rounded-xl flex flex-col">
-          <h1 className="px-4 pt-4 pb-2">{title}</h1>
+          <Link href={`/projects/${id}`}><h1 className="px-4 pt-4 pb-2">{title}</h1></Link>
           <h2 className={`px-4 pb-4 
           ${status == "Ongoing" ? "text-orange-500" : "text-red-500" | status == "Needs deco" ? "text-yellow-500" :  "text-red-500" | status == "Done" ? "text-green-500" : "text-red-500"}`}>
             <b>{status}</b>
