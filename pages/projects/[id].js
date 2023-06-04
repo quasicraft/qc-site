3443418f3518cea4e0dd537d64964fb909845258
Legoraft <50689727+legoraft@users.noreply.github.com>
2023-06-04 18:22:53 +0200

    made image wider

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -3,10 +3,12 @@ import Image from "next/image";
 
 export default function Project({ projectData }) {
   return (
-    <div className="mx-4 md:mx-auto prose prose-slate dark:prose-invert">
-        <Image className="object-cover" src={`https://ik.imagekit.io/quasicraft/tr:w-1280,h-360/${projectData.image}.png`} width={1280} height={360} />
-      <h1 className="pb-0 mb-0 text-center">{projectData.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: projectData.contentHtml }} />
+    <div>
+      <Image className="object-cover" src={`https://ik.imagekit.io/quasicraft/tr:w-1280,h-360/${projectData.image}.png`} width={1280} height={360} />
+      <div className="mx-4 md:mx-auto prose prose-slate dark:prose-invert">
+        <h1 className="pb-0 mb-0 text-center">{projectData.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: projectData.contentHtml }} />
+      </div>
     </div>
   );
 }
