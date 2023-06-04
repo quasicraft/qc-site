a4274cb636b097ef40cbf6682472d1e9219fc8c3
Legoraft <50689727+legoraft@users.noreply.github.com>
2023-06-04 21:39:01 +0200

    fixed status colors?

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -1,14 +1,15 @@
 import { getAllProjectsId, getProjectData } from "@/lib/projects";
 import Image from "next/image";
+import styles from "../../components/id.module.css";
 
 export default function Project({ projectData }) {
   return (
     <div>
       <Image className="object-cover" src={`https://ik.imagekit.io/quasicraft/tr:w-1280,h-360/${projectData.image}.png`} width={1280} height={360} />
-      <div className="pt-4 mx-4 md:mx-auto prose prose-slate dark:prose-invert">
-        <h1 className="pb-0 mb-0 text-center">{projectData.title}</h1>
-        <h3 className={`pb-0 pt-0 mt-0 mb-0 text-center ${projectData.status == "Ongoing" ? "text-orange-500" : "text-red-500" | projectData.status == "Needs deco" ? "text-yellow-500" :  "text-red-500" | projectData.status == "Done" ? "text-green-500" : "text-red-500"} font-bold`}>{projectData.status}</h3>
-        <div dangerouslySetInnerHTML={{ __html: projectData.contentHtml }} />
+      <div className="pt-4 mx-4 md:mx-auto prose">
+        <h1 className={styles.title}>{projectData.title}</h1>
+        <h3 color_code={projectData.status} className={styles.status}>{projectData.status}</h3>
+        <div className={styles.content} dangerouslySetInnerHTML={{ __html: projectData.contentHtml }} />
       </div>
     </div>
   );
