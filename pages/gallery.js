c99194e60ea992f074d41b7428f0106b1f365d5c
Legoraft <50689727+legoraft@users.noreply.github.com>
2023-06-04 14:23:12 +0200

    added image gallery

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,5 +1,20 @@
+import Image from "next/image";
+
 export default function Gallery() {
+
+  const images = [
+    "mob-switch", "main-storage-corner"
+  ]
+
+  const buildURL = imagePath => `https://ik.imagekit.io/quasicraft/${imagePath}.png`
+
   return(
-    <div>Hello, gallery!</div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-8">
+      {images.map(image => (
+        <div key={image} className="relative overflow-hidden flex flex-col rounded-xl m-2">
+          <Image src={buildURL(image)} width={1280} height={720} />
+        </div>
+      ))}
+    </div>
   );
 }
