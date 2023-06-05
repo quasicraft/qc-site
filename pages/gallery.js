f33c69d58d7ef6258033fab864c343c8cbe2c115
quasicraft <99789722+quasicraft@users.noreply.github.com>
2023-06-05 10:26:03 +0200

    added some bottm padding to gallery

--- pages/gallery.js
+++ pages/gallery.js
@@ -10,7 +10,7 @@ export default function Gallery() {
   const buildURL = imagePath => `https://ik.imagekit.io/quasicraft/tr:w-1280,h-720/${imagePath}.png`
 
   return(
-    <div className="grid grid-cols-1 md:grid-cols-2 mx-8">
+    <div className="grid grid-cols-1 md:grid-cols-2 mx-8 pb-2">
       {images.map(image => (
         <div key={image} className="relative overflow-hidden flex flex-col rounded-xl m-2">
           <Image src={buildURL(image)} width={1280} height={720} />
