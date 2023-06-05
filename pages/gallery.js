51b019aff955a4d257ffdc4f6eb5059bc9462aff
Legoraft <50689727+legoraft@users.noreply.github.com>
2023-06-05 18:12:10 +0200

    re-added bottom padding

--- pages/gallery.js
+++ pages/gallery.js
@@ -18,7 +18,7 @@ export default function photoGallery() {
 
   return(
     <Gallery>
-      <div className="relative grid grid-cols-1 md:grid-cols-2 mx-8">
+      <div className="relative grid grid-cols-1 md:grid-cols-2 mx-8 mb-4">
         {images.map(image => (
           <div className="overflow-hidden rounded-2xl m-2">
             <Item original={`https://ik.imagekit.io/quasicraft/${image}.png`} thumbnail={`https://ik.imagekit.io/quasicraft/tr:w-850,h-480/${image}.png`} width="1280" height="720">
