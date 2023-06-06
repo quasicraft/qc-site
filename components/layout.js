981c400c8595311b1ea9a8c2f533801ad609c726
Legoraft <50689727+legoraft@users.noreply.github.com>
2023-06-06 13:26:57 +0200

    added github link to footer

--- components/layout.js
+++ components/layout.js
@@ -5,8 +5,8 @@ import logo from "../public/images/icon.png";
 
 export default function Layout({ children }) {
   return(
-    <div className="flex flex-col min-h-screen bg-slate-950 text-slate-100 font-medium">
-      <header className="mx-4 mb-8 py-4">
+    <div className="flex flex-col min-h-screen bg-slate-950 text-slate-100">
+      <header className="mx-4 mb-8 py-4 font-medium">
         <div className="container mx-auto max-w-4xl flex justify-between">
           <Link href="/">
             <Image width="40" height="40" src={logo} />
@@ -25,8 +25,9 @@ export default function Layout({ children }) {
         </div>
       </header>
       <main className="container mx-auto max-w-4xl flex-1">{children}</main>
-      <footer className="mx-4 mb-8">
-        <div className='container mx-auto flex justify-center'>
+      <footer className="mx-4 mb-8 font-medium text-slate-400">
+        <div className='container mx-auto flex justify-between max-w-4xl'>
+            <Link className="hover:underline" href="https://github.com/quasicraft">Github</Link>
             &copy; 2023 Quasicraft
         </div>
       </footer>
