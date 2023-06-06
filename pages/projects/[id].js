1185bbac89a819dc5e401611a0183b081b6e7d43
quasicraft <99789722+quasicraft@users.noreply.github.com>
2023-06-06 11:17:53 +0200

    added dynamic tags for project pages

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -1,17 +1,38 @@
 import { getAllProjectsId, getProjectData } from "@/lib/projects";
 import Image from "next/image";
 import styles from "../../components/id.module.css";
+import Head from "next/head";
 
 export default function Project({ projectData }) {
   return (
-    <div>
+    <>
+      <Head>
+        <title>{projectData.title} - Quasicraft</title>
+        <meta 
+          name="description" 
+          content="Check out our gallery, with  amazing high-quality shots of all the beautiful builds on the quasicraft server." 
+          key="desc" 
+        />
+        <meta 
+          name="og:title" 
+          content={`${projectData.title} - Quasicraft`}
+        />
+        <meta 
+          name="og:description" 
+          content={`Current status of the ${projectData.title} project is: ${projectData.status}`}
+        />
+        <meta 
+          name="og:image" 
+          content={`https://ik.imagekit.io/quasicraft/tr:w-1280,h-360/${projectData.image}.png`}
+        />
+      </Head>
       <Image className="object-cover" src={`https://ik.imagekit.io/quasicraft/tr:w-1280,h-360/${projectData.image}.png`} width={1280} height={360} />
       <div className="pt-4 mx-4 md:mx-auto prose">
         <h1 className={styles.title}>{projectData.title}</h1>
         <h3 color_code={projectData.status} className={styles.status}>{projectData.status}</h3>
         <div className={styles.content} dangerouslySetInnerHTML={{ __html: projectData.contentHtml }} />
       </div>
-    </div>
+    </>
   );
 }
 
