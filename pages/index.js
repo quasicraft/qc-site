7b5b9cc4b5a871e8f81e94d56734bbb41b44e383
Legoraft <50689727+legoraft@users.noreply.github.com>
2023-06-06 13:27:43 +0200

    started prettyfying home page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import { getSortedProjectsData } from "@/lib/projects";
+import styles from "../components/projects.module.css";
 
-export default function Home() {
+export default function Home({ recentProjects }) {
   return(
     <>
       <Head>
@@ -27,7 +31,29 @@ export default function Home() {
           content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large"
         />
       </Head>
-      <div className="mx-8">Hello, world!</div>
+      <div className="mx-8">
+        <div id="recent-projects" className="mb-4">
+          <h1 className="text-3xl font-bold mb-4">Recent projects</h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {recentProjects.map(({ id, status, title }) => (
+              <div key={id} className="border-2 border-slate-800 rounded-xl flex flex-col">
+                <Link href={`/projects/${id}`}><h1 className="px-4 pt-4 pb-2">{title}</h1></Link>
+                <h2 color_code={status} className={styles.status}>{status}</h2>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const allProjectsData = getSortedProjectsData();
+  const recentProjects = allProjectsData.slice(0, 3);
+  return {
+    props: {
+      recentProjects,
+    },
+  };
 }
