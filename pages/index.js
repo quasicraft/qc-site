9700e5f5811c4f4d9db3c4de6262b75d2390f959
quasicraft <99789722+quasicraft@users.noreply.github.com>
2023-06-06 10:50:59 +0200

    added some meta tags

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
+import Head from "next/head";
+
 export default function Home() {
   return(
-    <div className="mx-8">Hello, world!</div>
+    <>
+      <Head>
+        <title>Quasicraft - Home</title>
+        <meta 
+          name="description" 
+          content="The website  for the Quasicraft server, with project overview and a gallery with all our projects." 
+          key="desc" 
+        />
+        <meta 
+          name="og:description" 
+          content="Website for Quasicraft wwith all the info on the server" 
+        />
+        <meta 
+          name="og:image" 
+          content="../public/images/social-image.png" 
+        />
+      </Head>
+      <div className="mx-8">Hello, world!</div>
+    </>
   );
 }
