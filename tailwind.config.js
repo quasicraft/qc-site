980d4d764f6d9106f4243dfaaa9f9d77fe582d6f
quasicraft <99789722+quasicraft@users.noreply.github.com>
2023-06-06 10:33:31 +0200

    added neww font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 module.exports = {
+
+  theme: {
+    fontFamily: {
+      sans:  [ "Manrope" ]
+    },
+  },
+
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   plugins: [ require('@tailwindcss/typography') ],
+  
 }
