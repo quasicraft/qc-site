9c7d8ee136b6d2c797b80b9475840b7f26b8ad82
quasicraft <99789722+quasicraft@users.noreply.github.com>
2023-06-06 10:53:23 +0200

    social image should be a link?

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         />
         <meta 
           name="og:image" 
-          content="../public/images/social-image.png" 
+          content="https://raw.githubusercontent.com/quasicraft/qc-site/807e5f317fde561d0e26c52b12d9f09bef762bd2/public/images/social-image.png" 
         />
       </Head>
       <div className="mx-8">Hello, world!</div>
