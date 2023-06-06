7f01eb293c5fea4f8b9923aeb4184b2ef17559c5
quasicraft <99789722+quasicraft@users.noreply.github.com>
2023-06-06 11:04:40 +0200

    added metadata to gallery page

--- pages/gallery.js
+++ pages/gallery.js
@@ -17,18 +17,40 @@ export default function photoGallery() {
   ]
 
   return(
-    <Gallery>
-      <div className="relative grid grid-cols-1 md:grid-cols-2 mx-8 mb-4">
-        {images.map(image => (
-          <div className="overflow-hidden rounded-2xl m-2">
-            <Item original={`https://ik.imagekit.io/quasicraft/${image}.png`} thumbnail={`https://ik.imagekit.io/quasicraft/tr:w-850,h-480/${image}.png`} width="1280" height="720">
-              {({ ref, open }) => (
-                <img ref={ref} onClick={open} src={`https://ik.imagekit.io/quasicraft/tr:w-850,h-480/${image}.png`} />
-              )}
-            </Item>
-          </div>
-        ))}
-      </div>
-    </Gallery>
+    <>
+      <Head>
+        <title>Quasicraft - Gallery</title>
+        <meta 
+          name="description" 
+          content="Check out our gallery, with  amazing high-quality shots of all the beautiful builds on the quasicraft server." 
+          key="desc" 
+        />
+        <meta 
+          name="og:title" 
+          content="Quasicraft - Gallery" 
+        />
+        <meta 
+          name="og:description" 
+          content="TCheck out our gallery, with  amazing high-quality shots of all the beautiful builds on the quasicraft server." 
+        />
+        <meta 
+          name="og:image" 
+          content="https://raw.githubusercontent.com/quasicraft/qc-site/807e5f317fde561d0e26c52b12d9f09bef762bd2/public/images/social-image.png" 
+        />
+      </Head>
+      <Gallery>
+        <div className="relative grid grid-cols-1 md:grid-cols-2 mx-8 mb-4">
+          {images.map(image => (
+            <div className="overflow-hidden rounded-2xl m-2">
+              <Item original={`https://ik.imagekit.io/quasicraft/${image}.png`} thumbnail={`https://ik.imagekit.io/quasicraft/tr:w-850,h-480/${image}.png`} width="1280" height="720">
+                {({ ref, open }) => (
+                  <img ref={ref} onClick={open} src={`https://ik.imagekit.io/quasicraft/tr:w-850,h-480/${image}.png`} />
+                )}
+              </Item>
+            </div>
+          ))}
+        </div>
+      </Gallery>
+    </>
   );
 }
